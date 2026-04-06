00dde860c90068d398be67f27b67a78b9ab6b66f
Alex Vasquez <alexorde12@gmail.com>
2026-04-06 05:09:53 -0500

    Removing no needed data.

--- TransmitterApi.Api/Program.cs
+++ TransmitterApi.Api/Program.cs
@@ -59,8 +59,3 @@ app.MapGet("/products", async (IProductService productService) =>
 .WithName("GetProducts");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
